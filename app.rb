c5cde962fcb1cdc338dbc8561c92341322cf41ab
Benjamin Williams <benjamin.joseph.williams@gmail.com>
2015-03-31 13:58:44 +0100

    Add some admin views

--- app.rb
+++ app.rb
@@ -1,18 +1,31 @@
-require 'sinatra'
-require './url_store.rb'
-require './analytics.rb'
+require 'sinatra/base'
 
-not_found do
-  "not found"
+class URLShortenerAdmin < Sinatra::Base
+  get '/admin' do
+    slim :show, layout: :'layouts/index'
+  end
+
+  get '/admin/create' do
+    slim :create, layout: :'layouts/index'
+  end
 end
 
-get /(\w+)/ do
-  shortened_url = params['captures']
-  full_url = URLStore.find(shortened_url)
-  if full_url
-    Analytics.add_to_counter(shortened_url)
-    redirect full_url
-  else
-    halt 404
+class URLShortener < Sinatra::Base
+  require './url_store.rb'
+  use URLShortenerAdmin
+
+  not_found do 
+    "not found"
+  end
+
+  get /(\w+)/ do
+    shortened_url = params['captures']
+    full_url = URLStore.find(shortened_url)
+    if full_url
+      Analytics.add_to_counter(shortened_url)
+      redirect full_url
+    else
+      halt 404 
+    end
   end
 end
