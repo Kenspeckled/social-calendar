077091a6a36291b53385389d2960302e7293ec42
Richard <richard.t.bell90@gmail.com>
2015-04-03 14:59:13 +0100

    Add some calendar views

--- README.md
+++
@@ -1,1 +0,0 @@
-Needs a file called config.yml that defines a `session_secret`
--- analytics.rb
+++
@@ -1,9 +0,0 @@
-module Analytics
-
-  @redis = Redis.new
-
-  def self.add_to_visit_count(key)
-    @redis.hincrby("key:#{key}", "visit_count", 1)
-  end
-
-end
