fee7351adbfc9ae0d899d7ef4a7ae8c40bcbc825
DagmaraSz <daga.sz@hotmail.com>
2015-04-03 15:02:11 +0000

    initial scheduler set up

---
+++ scheduler.rb
@@ -0,0 +1,30 @@
+class Scheduler
+  attr_reader :jobs
+ 
+  #we'll get this from the database
+  def initialise
+    a = {time: Time.now, content:"Hello", service: :twitter}
+    @messages = [a]
+  end
+    
+  def reccur
+    messages.select do |message|
+      message_time = minutes_since_epoch(message[:time])
+      current_time = minutes_since_epoch(Time.now)
+      if message_time == current_time
+        SocialPlatform.send(message)
+      end
+    end
+    sleep 1
+    recurr
+  end
+  
+  private 
+  
+  def minutes_since_epoch(time_string)
+    time_string.to_i / 60
+  end
+    
+    
+  
+end
