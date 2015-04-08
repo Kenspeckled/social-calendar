b33ce22991cab68d79417d76369047e6c95f504b
Benjamin Williams <benjamin.joseph.williams@gmail.com>
2015-04-08 09:35:27 +0100

    Path to secrets file

--- lib/twitter_api_wrapper.rb
+++ lib/twitter_api_wrapper.rb
@@ -13,7 +13,7 @@ class TwitterAPIWrapper
   private
 
   def self.create_access_token
-    secrets = YAML.load_file('./secrets.yml')
+    secrets = YAML.load_file(File.join(File.dirname(__FILE__),'../secrets.yml'))
     consumer = OAuth::Consumer.new(secrets['API_Key'], secrets['API_Secret'], { site: "http://api.twitter.com", scheme: :header })
     OAuth::AccessToken.from_hash(consumer, { oauth_token: secrets['oauth_token'], oauth_token_secret: secrets['oauth_token_secret'] })
   end
